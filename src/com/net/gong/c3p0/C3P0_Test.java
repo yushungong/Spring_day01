525d4ad58fca05ffedbf70e542b084fd69b0b8ca
yushungong <1042057924@qq.com>
2018-02-25 08:45:31 +0800

    aa

--- src/com/net/gong/c3p0/C3P0_Test.java
+++ src/com/net/gong/c3p0/C3P0_Test.java
@@ -10,18 +10,21 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 // 该注解开启事务，加上注解的所有方法都会开启事务
 public class C3P0_Test {
 	public static void main(String[] args) {
-		
+		try {
+			test1();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	//c3p0连接池手动配置方法
-	@SuppressWarnings("unused")
 	private static void test1() throws Exception{
 		ComboPooledDataSource dataSource = new ComboPooledDataSource();
-		dataSource.setDriverClass("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		dataSource.setJdbcUrl("sqlserver://db.rnd.cas-pe.com:1433;databaseName=CAS_PRD_PAS_DATA_SC");
+		dataSource.setDriverClass("com.microsoft.sqlserver.jdbc.SQLServerDriverr");
+		dataSource.setJdbcUrl("jdbc:sqlserver://127.0.0.1:1433;DatabaseName=MYDB_HOME");
 		dataSource.setUser("sa");
-		dataSource.setPassword("123456");
+		dataSource.setPassword("Password_1462");
 		Connection con =  dataSource.getConnection();
-		con.prepareStatement("sql");
+		con.prepareStatement("INSERT INTO [emp] ([eid], [ename], [esal], [esex]) VALUES ('1', 'aa', '3000', '男');");
 	}
 }
